bc1c2e8ffdf2981a763c63acfb75cb2ca7106a3d
Janice Patricia <janice.patricia@oicr.on.ca>
2016-08-18 10:54:49 -0400

    worfklow Details test cypress

--- cypress/integration/workflowDetails.js
+++ cypress/integration/workflowDetails.js
@@ -1,23 +1,17 @@
 
-'use strict';
-
-/* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
-
-var page = function(){
-  this.button = element(by.id("editButton"));
-};
-
 describe('Dockstore Workflow Details', function() {
-  var Page = new page();
 
 	beforeEach(function () {
-    browser.ignoreSynchronization = true; //to avoid error angular is not found --> could be because of syncing problem
-		browser.get('workflows/DockstoreTestUser/hello-dockstore-workflow');
-  });
 
-	it('should not have Edit button', function() {
+  })
+
+	it('should not show Edit Button', function() {
+    cy.visit("http://10.0.29.14:9001/workflows/DockstoreTestUser/hello-dockstore-workflow")
     // edit button should only appear inside "My Workflows"
     // unless logged in as the author, edit button should not be present in "Workflows"
-    expect(Page.button.isPresent()).toBe(false);
-  });
-});
+    cy
+      .get("#editButton")
+        .should("exist")
+        .should("not.be.visible")
+  })
+})
