317d5ca7e9ab0bfe9f5840e02bfda35f153ca08a
Janice Patricia <janice.patricia@oicr.on.ca>
2016-08-18 11:46:05 -0400

    try to make server run first before cypress

--- cypress/integration/app-spec.js
+++ cypress/integration/app-spec.js
@@ -6,11 +6,10 @@
 describe('dockstore homepage', function() {
 
 	beforeEach(function () {
-		
+		cy.visit("http://localhost:9001");
 	});
 
 	it('cy.should - assert that <title> is correct', function() {
-		cy.visit("http://localhost:9001/index.html");
 		cy.title().should('include', 'Dockstore');
 	  // ignoring for now, not working in combination with API display
 		//expect(browser.getLocationAbsUrl()).toMatch("/");
